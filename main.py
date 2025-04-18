9482076ac66fa20ab148c88f2699d826b4d8be3b
programmerkenzi <a23g32@gmail.com>
2025-04-18 11:27:32 +0800

    Initialize voice setup and greeting in main execution flow

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# joystick_camera_control.py
 import threading
 from modules.joystick import joystick_control
 from modules.camera import camera_stream
+from modules.voice import setup_voice, say
 
 if __name__ == "__main__":
+    setup_voice()
+    say("Hello! This is PiCrawler.")
     t1 = threading.Thread(target=joystick_control)
     t2 = threading.Thread(target=camera_stream)
     t1.start()
